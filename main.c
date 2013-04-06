bf1c6064ee10005553ae4ca5a95504c2de0aab15
Adam Monsen <haircut@gmail.com>
2013-04-06 11:12:33 -0700

    Prep for bisect demo: add a program and a test

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * This function takes no input and always returns five.
+ */
+int f1(void) {
+    return 5;
+}
+
+/*
+ * This function takes no input and always returns zero.
+ */
+int f2(void) {
+    return 0;
+}
+
+/*
+ * This function decides what to return based on f1() and f2().
+ */
+int main(void)
+{
+    return f1() - 5 + f2();
+}
